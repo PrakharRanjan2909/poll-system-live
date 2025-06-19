ea4bb443f127fabc5d821c7e5b7c8f85d64c2359
PrakharRanjan2909 <rprakhar007@gmail.com>
2025-06-19 13:47:54 +0530

    deploy2

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const http = require("http");
-require("dotenv").config();
-const { Server } = require("socket.io");
-const { TeacherLogin } = require("./controllers/login");
-const {
-  createPoll,
-  voteOnOption,
-  getPolls,
-} = require("../src/controllers/poll");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import http from "http";
+import dotenv from "dotenv";
+dotenv.config();
+import { Server } from "socket.io";
+import { TeacherLogin } from "../src/controllers/teacher";
+import { createPoll, voteOnOption, getPolls } from "../src/controllers/poll";
 
-const path = require("path");
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const DB =
   process.env.NODE_ENV === "production"
